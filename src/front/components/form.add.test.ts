f7a6fd516b38c95953b28ef66b1277d9f5ab1c7f
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-05-26 12:02:16 +0100

    Add createFormAdd tests for form submit

--- src/front/components/form.add.test.ts
+++ src/front/components/form.add.test.ts
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/dom';
-//import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { createFormAdd } from './form.add';
 
@@ -20,11 +20,44 @@ describe('GIVEN <createFormAdd> component', () => {
         expect(screen.getByLabelText(/Name/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/Description/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/Price/i)).toBeInTheDocument();
-        expect(screen.getByLabelText(/Esta en promoción/i)).toBeInTheDocument();
+        expect(screen.getByLabelText(/promoción/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/Category/i)).toBeInTheDocument();
 
         expect(
             screen.getByRole('button', { name: /Crear/i }),
         ).toBeInTheDocument();
+    });
+
+    test('It should call handleSubmit with form data', async () => {
+        const spy = vi.spyOn(console, 'log');
+
+        // const form = screen.getByRole('form', { name: /add/i });
+
+        await userEvent.type(screen.getByLabelText(/Name/i), 'Patata');
+        await userEvent.type(
+            screen.getByLabelText(/Description/i),
+            'Las mejores patatas de la comarca',
+        );
+        await userEvent.type(screen.getByLabelText(/Price/i), '1000');
+        await userEvent.click(screen.getByLabelText(/promoción/i));
+        await userEvent.selectOptions(
+            screen.getByLabelText(/Category/i),
+            'components',
+        );
+
+        await userEvent.click(screen.getByRole('button', { name: /Crear/i }));
+        expect(spy).toBeCalled();
+
+        // expect(spy).toHaveBeenCalledWith(
+        //     'Form submitted:',
+        //     expect.objectContaining({
+        //         id: 1,
+        //         name: 'Patata',
+        //         description: 'Una Las mejores patatas de la comarca',
+        //         price: 1000,
+        //         hasPromo: true,
+        //         category: 'components',
+        //     }),
+        // );
     });
 });
