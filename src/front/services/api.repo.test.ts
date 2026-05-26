8f1fbe9066e2a1185a2ffb84ef1ce9b4154b92af
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-05-26 12:35:42 +0100

    Add tests for getProducts method in ApiRepo class

--- src/front/services/api.repo.test.ts
+++ src/front/services/api.repo.test.ts
@@ -8,4 +8,33 @@ describe('GIVEN <ApiRepo> class', () => {
             expect(repo).toBeInstanceOf(ApiRepo);
         });
     });
+
+    describe('WHEN the method <getProducts> is called', () => {
+        describe('AND response is OK', () => {
+            beforeEach(() => {
+                vi.spyOn(globalThis, 'fetch').mockResolvedValue({
+                    ok: true,
+                    json: vi.fn().mockResolvedValueOnce([]),
+                } as unknown as Response);
+            });
+
+            test('THEN it should return the fetch data', async () => {
+                const products = await repo.getProducts();
+                expect(fetch).toHaveBeenCalled();
+                expect(products).toBeInstanceOf(Array);
+            });
+        });
+
+        describe('AND response is NOT OK', () => {
+            beforeEach(() => {
+                vi.spyOn(globalThis, 'fetch').mockRejectedValue({
+                    ok: false,
+                }) as unknown as Response;
+            });
+
+            test('THEN it should reject the promise', () => {
+                expect(repo.getProducts()).rejects.toThrow();
+            });
+        });
+    });
 });
